c27c1c5c41ad08455a461b4abcbb86e44a589e8a
ryu-0406 <r.yoshida0604@gmail.com>
2021-07-20 17:25:16 +0200

    variant_lesson

---
+++ variable.py
@@ -0,0 +1,11 @@
+num = 1
+name = 'Mike'
+is_ok = True
+number = '1'
+new_number = int(number)
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+print(number, type(number))
+print(new_number, type(new_number))
