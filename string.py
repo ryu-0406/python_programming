1e2d1084ce40bbf6f559ef727824de24a99d21e5
ryu-0406 <r.yoshida0604@gmail.com>
2021-07-20 17:49:25 +0200

    String lesson

---
+++ string.py
@@ -0,0 +1,21 @@
+x = 'test'
+print('hellp world')
+print("I don't know")
+print('I don\'t know')
+print('hello. \nHow are you')
+print(r'C:\nam\name')
+
+print("#############")
+print("""\
+line1
+line2
+line3\
+""")
+print("#############")
+
+print('Hi.' * 3)
+
+print('Py'+'thon')
+print('Py''thon')
+prefix = 'Py'
+print(prefix+ 'thon')
