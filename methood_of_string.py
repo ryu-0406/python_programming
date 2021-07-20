db6fbbbf1764f921d2067d2e268e4751f8f398c1
ryu-0406 <r.yoshida0604@gmail.com>
2021-07-20 18:11:41 +0200

    String lesson

--- methood_of_string.py
+++ methood_of_string.py
@@ -1,1 +1,13 @@
-print(11111)
+s = ('My name is Mike. Hi Mike')
+is_start = s.startswith('My')
+
+print(is_start)
+
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
