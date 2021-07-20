288aee8d77e47fe7678b768f8a27c4261caa6961
ryu-0406 <r.yoshida0604@gmail.com>
2021-07-20 18:01:37 +0200

    String lesson

--- string.py
+++ string.py
@@ -19,3 +19,24 @@ print('Py'+'thon')
 print('Py''thon')
 prefix = 'Py'
 print(prefix+ 'thon')
+
+s = ('aaaaaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbbbbb')
+print(s)
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print("#############")
+print(word[0:2])
+print(word[2:5])
+print("#############")
+print(word[:2])
+print(word[2:])
+print("#############")
+word = 'j' + word[1:]
+print(word)
+print(word[:])
+n = len(word)
+print(n)
