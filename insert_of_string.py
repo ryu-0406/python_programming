7fe338066109b329998ef6f7f61fe4bd357e00a6
ryu-0406 <r.yoshida0604@gmail.com>
2021-07-20 18:38:32 +0200

    insert of string

--- insert_of_string.py
+++ insert_of_string.py
@@ -6,3 +6,9 @@ print(y)
 
 z = 'My name is {0} {1}'.format('Jun', 'Sakai')
 print(z)
+
+a = 'My name is {name} {family}. Watashi ha {family} {name}'.format(name='Jun', family='Sakai')
+print(a)
+
+num = str(1)
+print(num, type(num))
